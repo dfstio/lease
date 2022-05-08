ca373a3e9bd7074c065c1df14c7c897054efa1db
Benjamin Herald <bherald@pm.me>
2022-05-08 03:26:26 +0300

    application 2

--- packages/cli/application.js
+++ packages/cli/application.js
@@ -19,10 +19,10 @@ async function main()
 		 await tree.insert(hash, LesseeJSON[attributename]);
 	 }
 	 const root = tree.F.toObject(tree.root);
-	 console.log("Root: ", root.toString());
+	 console.log("Calculated roothash: ", root.toString());
 	 let newJSON = LesseeJSON;
 	 newJSON.roothash = root;
-	 console.log("NewJSON: ", newJSON);
+	 //console.log("NewJSON: ", newJSON);
 	 fs.writeFile("../../data/lessee-input.json", JSON.stringify(newJSON, (_, v) => typeof v === 'bigint' ? v.toString() : v), function (err) {
 		   if (err) return console.log(err);
 		 });
