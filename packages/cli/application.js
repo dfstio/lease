2aebf94788598f73b141df3a85d9f8eec3154027
Benjamin Herald <bherald@pm.me>
2022-05-08 03:20:08 +0300

    application

---
+++ packages/cli/application.js
@@ -0,0 +1,31 @@
+const LesseeJSON = require("../../data/lessee-data.json");
+const keccak256 = require('keccak256');
+const fs = require('fs');
+const newMemEmptyTrie = require("circomlibjs").newMemEmptyTrie;
+
+
+main();
+
+
+async function main()
+{
+	 let tree;
+	 tree = await newMemEmptyTrie();
+
+	 for(var attributename in LesseeJSON)
+	 {
+		 console.log(attributename+": "+LesseeJSON[attributename]);
+		 const hash = keccak256(attributename);
+		 await tree.insert(hash, LesseeJSON[attributename]);
+	 }
+	 const root = tree.F.toObject(tree.root);
+	 console.log("Root: ", root.toString());
+	 let newJSON = LesseeJSON;
+	 newJSON.roothash = root;
+	 console.log("NewJSON: ", newJSON);
+	 fs.writeFile("../../data/lessee-input.json", JSON.stringify(newJSON, (_, v) => typeof v === 'bigint' ? v.toString() : v), function (err) {
+		   if (err) return console.log(err);
+		 });
+	 
+	 
+};
